98e20db451ab11a36e86b5e091178d9c158006a5
Miguel M <miguel.re@unochapeco.edu.br>
2025-09-17 19:48:42 -0300

    Convert_System_ok

--- Scritpts.js
+++ Scritpts.js
@@ -1,3 +1,57 @@
-const USD = 5.78
+const USD = 5.37
 const EUR = 6.34
 const GBP = 7.37  
+
+
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const result = document.getElementById("description")
+
+amount.addEventListener("input", () =>{
+
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, '')
+  console.log(amount.value)
+})
+
+form.onsubmit = (e) => {
+  e.preventDefault()
+
+  switch(currency.value){
+    case "USD":
+      convert(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convert(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convert(amount.value, GBP, "£")
+      break
+    default:
+      alert("Selecione uma moeda")
+      break
+  }
+
+}
+function convert(amount, price, symbol){
+  try{
+    description.textContent = `${symbol} 1 = ${price}`
+      footer.classList.add("show-footer")
+
+  }catch(error){
+    console.log(error)
+
+    footer.classList.remove("show-footer")
+    alert("Ocorreu um erro ao converter a moeda, Tente novamente mais tarde!!!")
+
+  }
+
+}
+
+function formatCurrency(value){
+  return Number(value).toLocaleString('pt-BR', {
+    style: 'currency', 
+    currency: 'BRL'})
+}
