b2121751ba7e0cdeba4b322d7a24ddaf1adb4d76
Miguel M <miguel.re@unochapeco.edu.br>
2025-09-30 22:58:09 -0300

    Add os comnetários ao arquivo JS

--- Scritpts.js
+++ Scritpts.js
@@ -1,8 +1,10 @@
+// Taxas de câmbio fixas
+// Em busca de futuras APIs
 const USD = 5.37
 const EUR = 6.34
 const GBP = 7.37  
 
-
+// Selecionando elementos do DOM/FORMULÀRIO
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
@@ -10,17 +12,25 @@ const footer = document.querySelector("main footer")
 const description = document.getElementById("description")
 const result = document.getElementById("result")
 
+// Adicionando evento de input ao campo de valor
 amount.addEventListener("input", () =>{
 
+  // Regex para permitir apenas números e vírgulas  
   const hasCharactersRegex = /\D+,[. ,]+/g
+  // Substituindo caracteres inválidos
   amount.value = amount.value.replace(hasCharactersRegex, "")
 
   console.log(amount.value)
 })
 
-form.onsubmit = (e) => {
-  e.preventDefault()
+// capturando evento de submit do formulário
 
+form.onsubmit = (e) => {
+
+  // Prevenindo o comportamento padrão do formulário
+  e.preventDefault()
+  
+  //switch para selecionar a moeda
   switch(currency.value){
     case "USD":
       convert(amount.value, USD, "US$")
@@ -37,14 +47,20 @@ form.onsubmit = (e) => {
   }
 
 }
+
+// Função para converter o valor
+
 function convert(amount, price, symbol){
+  // Tratando possíveis erros com try...catch
   try{
       
+    // Exibindo o resultado da conversão
     description.textContent = `${symbol} 1 = ${price}`
       footer.classList.add("show-result")
 
+    // Função para calcular o valor convertido
       function calculate(amount){
-
+        
       if(currency.value == "USD"){
          amount = amount * USD
          result.textContent = `${amount} Reais`
@@ -57,21 +73,23 @@ function convert(amount, price, symbol){
         }else{
           alert("Selecione uma moeda")
         }
+        // Formatando o valor para o padrão brasileiro
         result.textContent = formatCurrency(amount)
       }
     calculate(amount)
     
-
+      
   }catch(error){
     console.log(error)
 
+    // Em caso de erro, removo a exibição do resultado e mostro um alerta
     footer.classList.remove("show-result")
     alert("Ocorreu um erro ao converter a moeda, Tente novamente mais tarde!!!")
 
   }
 }
 
-
+// Função para formatar o valor para o padrão brasileiro
 function formatCurrency(value){
   return Number(value).toLocaleString('pt-BR', {
     style: 'currency', 
